909705ffc9d8dd181dc9a4c0b8e4524092d2c63e
ozzy <ozzy>
2015-12-15 16:36:06 +0200

    clean SPA app

--- public/javascript/createBooksList.js
+++ public/javascript/createBooksList.js
@@ -1,15 +1,38 @@
 $(function () {
 
-    //Get JSON data from server using an AJAX HTTP GET request
     $.getJSON("http://localhost:8083/getbook", function(data){
-        var orderedListOfBooks = $('ol.bookList');
 
-        $.each(data, function (i, bookItem) {
+        var table = $('table.bookList');
+        var tableRow = '';
+        var count = 0;
 
-            var listItem = $('<li/>')
-                .appendTo(orderedListOfBooks)
-                .text(bookItem.author+' '+'"'+bookItem.title+'"');
-
+        $.each(data, function (index, item) {
+            tableRow += '<tr><td>' + ++index +
+                        '</td><td>' + item.author +
+                        '</td><td>' + item.title +
+                        '</td><td>' + item.published +
+                        '</td><td>' + item.genre +
+                        '</td><td><a href="#" class="deleteItem" rel="' + this._id + '"><img src="../i/trash.png" alt="Delete"/></a>' +
+                                 '<br/><a href="#" class="updateItem" rel="' + this._id + '"><img src="../i/edit.png" alt="Edit"/></a></td></tr>';
+            count = index;
         });
+        var lastRow = '<tr><td class="index" >' + ++count + '</td>' +
+            '<td class="author" contenteditable="true"></td>' +
+            '<td class="title" contenteditable="true"></td>' +
+            '<td class="published" contenteditable="true"></td>'+
+            '<td class="genre" contenteditable="true"></td>'+
+            '<td><a href="#" class="addItem"><img src="../i/add.png" alt="Add"/></a></td></tr>';
+
+        table.append(tableRow);
+        table.append(lastRow);
     });
+
 });
+
+
+
+
+
+
+
+
