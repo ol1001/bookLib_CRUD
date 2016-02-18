25a1a850a3c12626d0a359e893f148547d04c9a5
Olha Stratii <shmelova83@gmail.com>
2016-02-18 12:11:36 +0200

    change  path

--- public/javascript/src/createBooksList.js
+++ public/javascript/src/createBooksList.js
@@ -10,10 +10,10 @@ $(function () {
             tableRow += '<tr><td>' + ++index +
             '</td><td>' + item.author +
             '</td><td><a href="#" class="open">' + item.title +
-            '<img class="open" src="../../i/open.png"></a></td><td>' + item.published +
+            '<img class="open" src="../../img/open.png"></a></td><td>' + item.published +
             '</td><td>' + item.genre +
-            '</td><td><a href="#" class="deleteItem" rel="' + this._id + '"><img src="../../i/trash.png" alt="Delete"/></a>' +
-            '<br/><a href="#" class="updateItem" rel="' + this._id + '"><img src="../../i/edit.png" alt="Edit"/></a></td></tr>';
+            '</td><td><a href="#" class="deleteItem" rel="' + this._id + '"><img src="../../img/trash.png" alt="Delete"/></a>' +
+            '<br/><a href="#" class="updateItem" rel="' + this._id + '"><img src="../../img/edit.png" alt="Edit"/></a></td></tr>';
             count = index;
         });
         var lastRow = '<tr><td class="index" >' + ++count + '</td>' +
@@ -21,8 +21,8 @@ $(function () {
             '<td class="title" contenteditable="true"></td>' +
             '<td class="published" contenteditable="true"></td>' +
             '<td class="genre" contenteditable="true"></td>' +
-            '<td><a href="#" class="addItem"><img src="../../i/add.png" alt="Add"/></a><br/>' +
-            '<a href="#" class="uploadItem"><img src="../../i/upload.png" alt="Upload"/></a></td></tr>';
+            '<td><a href="#" class="addItem"><img src="../../img/add.png" alt="Add"/></a><br/>' +
+            '<a href="#" class="uploadItem"><img src="../../img/upload.png" alt="Upload"/></a></td></tr>';
 
         table.append(tableRow);
         table.append(lastRow);
